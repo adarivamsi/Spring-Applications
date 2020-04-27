a99dec028f3f12bfb42129b41a27badfdc14f74c
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-04-27 09:40:22 -0400

    Added Repo and Interface.

---
+++ Conference/src/main/java/com/adarivamsi/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,30 @@
+package com.adarivamsi.repository;
+
+import com.adarivamsi.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * All Copyrights reserved @ Vamsi Charan Adari
+ * Any queries mail me at adarivamsi@gmail.com
+ * Created by adari on 4/27/2020
+ */
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    public List<Speaker> findAll() {
+
+        List<Speaker> speakers = new ArrayList<Speaker>();
+
+        Speaker speaker = new Speaker();
+
+        speaker.setFirstName("Vamsi Charan");
+        speaker.setLastName("Adari");
+
+        speakers.add(speaker);
+
+        return speakers;
+
+    }
+
+}
